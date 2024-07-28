63f662c017fabe7b027752c61cfe40f26013eaea
homoonshi <homoonshi@gmail.com>
2024-07-28 09:16:06 +0900

    Fix : WebSocket Config 일부 수정

--- feedme/src/main/java/com/todoslave/feedme/repository/MemberChatRoomRepository.java
+++ feedme/src/main/java/com/todoslave/feedme/repository/MemberChatRoomRepository.java
@@ -4,12 +4,14 @@ package com.todoslave.feedme.repository;
 import com.todoslave.feedme.domain.entity.communication.MemberChatRoom;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 public interface MemberChatRoomRepository extends MongoRepository<MemberChatRoom, String> {
 
   // 방 번호 얻어오기
-  MemberChatRoom findByParticipantIdsContaining(List<String>participantIds);
+  @Query("{ 'participantIds': ?0 }")
+  MemberChatRoom findByParticipantIdsContainingAll(List<String> participantIds);
 
   // save 메서드는 이미 몽고디비에 존재하므로 재 정의 할 필요 X
 
