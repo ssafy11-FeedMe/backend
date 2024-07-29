3a5391addddb40764ebcf87c2146d39e0703a8d2
homoonshi <homoonshi@gmail.com>
2024-07-29 23:40:57 +0900

    Feat : Friend CRUD 완료

--- feedme/src/main/java/com/todoslave/feedme/controller/FriendController.java
+++ feedme/src/main/java/com/todoslave/feedme/controller/FriendController.java
@@ -1,14 +1,89 @@
 package com.todoslave.feedme.controller;
 
+import com.todoslave.feedme.domain.entity.communication.Friend;
+import com.todoslave.feedme.domain.entity.communication.FriendRequest;
+import com.todoslave.feedme.service.FriendService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/friends")
 public class FriendController {
 
+    static private FriendService friendService;
+
+    // 친구 추가하기
+    @PostMapping
+    public ResponseEntity<Void> addFriend(@RequestParam("token") String token, @RequestParam("counterEmail") String email){
+
+        // token으로부터 memberId를 찾는 로직
+        int memberId;
+
+        friendService.insertFriend(memberId, email);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // 친구 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeFriend(@RequestParam("token") String token, @RequestParam("id") String friendId){
+
+        // token으로부터 memberId를 찾는 로직
+        int memberId;
+
+        friendService.deleteFriend(friendId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // 친구 목록 불러오기
+    @GetMapping
+    public ResponseEntity<List<Friend>> findFriends(@RequestParam("token") String token){
+        //token으로부터 memberId를 찾는 로직
+        int memberId;
+
+        return ResponseEntity.ok(friendService.getFriends(memberId));
+    }
+
+    // 친구 요청 목록 조회
+    @GetMapping("/request")
+    public ResponseEntity<List<FriendRequest>> findRequestFriend(@RequestParam("token") String token){
+        //token으로부터 memberId를 찾는 로직
+        int memberId;
+
+        return ResponseEntity.ok(friendService.getRequestFriend(memberId));
+    }
+
+    // 친구 수락하기
+    @PostMapping("/{id}/accept")
+    public ResponseEntity<Void> acceptFriendship(@RequestParam("token") String token, @PathVariable("id") int requestId){
+        // token 인증
+        if(true) {
+            friendService.insertFriendship(requestId);
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // 친구 거절하기
+    @PostMapping("/{id}/reject")
+    public ResponseEntity<Void> rejectFriendship(@RequestParam("token") String token, @PathVariable("id") int requestId){
+        // token 인증
+        if(true){
+            friendService.deleteRequestFriend(requestId);
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // 친구 활동 피드
+
+    // 친구 일정 공유
 
 
 }
--- feedme/src/main/java/com/todoslave/feedme/service/FriendService.java
+++ feedme/src/main/java/com/todoslave/feedme/service/FriendService.java
@@ -1,7 +1,28 @@
 package com.todoslave.feedme.service;
 
+import com.todoslave.feedme.domain.entity.communication.Friend;
+import com.todoslave.feedme.domain.entity.communication.FriendRequest;
+
+import java.util.List;
+
 public interface FriendService {
 
+    // 친구 추가
+    void insertFriend(int memberId, String email);
 
+    // 친구 삭제
+    void deleteFriend(int friendId);
+
+    // 친구 목록 조회
+    List<Friend> getFriends(Integer memberId);
+
+    // 친구 요청 목록 조회
+    List<FriendRequest> getRequestFriend(Integer memberId);
+
+    // 친구 수락
+    void insertFriendship(int requestId);
+
+    // 친구 거절
+    void deleteRequestFriend(int requestId);
 
 }
