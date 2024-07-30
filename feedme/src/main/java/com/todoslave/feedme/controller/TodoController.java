204ca35596a2cfc7702dd74d816fcdf2076dc10e
김문희 <homoonshi@gmail.com>
2024-07-30 09:49:05 +0900

    Fix : JWT Token RequestHeader 로 수정

--- feedme/src/main/java/com/todoslave/feedme/controller/TodoController.java
+++ feedme/src/main/java/com/todoslave/feedme/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,30 +27,30 @@ public class TodoController {
   private final TodoService todoservice;
 
   @GetMapping("/daily")
-  public ResponseEntity<List<Todo>> findTodoDaily(@RequestBody Todo todo){
+  public ResponseEntity<List<Todo>> findTodoDaily(@RequestHeader("Authorization") String token, @RequestBody Todo todo){
     Timestamp timestamp = todo.getCreatedAt();
     Date createdAt = new Date(timestamp.getTime());
     return ResponseEntity.ok(todoservice.getTodoDaily(todo.getMember().getId(), createdAt));
   }
 
   @PostMapping
-  public ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
+  public ResponseEntity<Todo> createTodo(@RequestHeader("Authorization") String token, @RequestBody Todo todo){
     return ResponseEntity.ok(todoservice.insertTodo(todo));
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> removeTodo(@PathVariable int id){
+  public ResponseEntity<Void> removeTodo(@RequestHeader("Authorization") String token, @PathVariable int id){
     todoservice.deleteTodo(id);
     return ResponseEntity.noContent().build();
   }
 
   @PatchMapping()
-  public ResponseEntity<Todo> modifyTodo(@RequestBody Todo todo){
+  public ResponseEntity<Todo> modifyTodo(@RequestHeader("Authorization") String token, @RequestBody Todo todo){
     return ResponseEntity.ok(todoservice.updateTodoContent(todo.getId(), todo.getContent()));
   }
 
   @PostMapping("/{id}/complete")
-  public ResponseEntity<Todo> completeTodo(@PathVariable int id){
+  public ResponseEntity<Todo> completeTodo(@RequestHeader("Authorization") String token, @PathVariable int id){
     return ResponseEntity.ok(todoservice.completeTodo(id));
   }
 
