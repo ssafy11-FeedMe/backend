01e29c9eeef0e4b47b33b5b96bd87d62bc6e67da
김문희 <homoonshi@gmail.com>
2024-07-31 15:36:33 +0900

    Feat : Alarm 일부 추가

---
+++ feedme/src/main/java/com/todoslave/feedme/service/AlarmServiceImpl.java
@@ -0,0 +1,14 @@
+package com.todoslave.feedme.service;
+
+
+import com.todoslave.feedme.repository.AlarmRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AlarmServiceImpl implements AlarmService{
+
+  private AlarmRepository alarmRepository;
+
+
+
+}
