95a2f75c454a46e7e6297bfdb166407301f5f9af
김문희 <homoonshi@gmail.com>
2024-08-06 10:49:59 +0900

    Fix : friend 기능 수정

--- feedme/src/main/java/com/todoslave/feedme/controller/FriendController.java
+++ feedme/src/main/java/com/todoslave/feedme/controller/FriendController.java
@@ -1,9 +1,14 @@
 package com.todoslave.feedme.controller;
 
+import com.todoslave.feedme.DTO.FriendReqRequestDTO;
+import com.todoslave.feedme.DTO.FriendReqResponseDTO;
+import com.todoslave.feedme.DTO.FriendResponseDTO;
 import com.todoslave.feedme.domain.entity.communication.Friend;
 import com.todoslave.feedme.domain.entity.communication.FriendRequest;
+import com.todoslave.feedme.domain.entity.membership.Member;
 import com.todoslave.feedme.service.AlarmService;
 import com.todoslave.feedme.service.FriendService;
+import com.todoslave.feedme.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,77 +20,49 @@ import java.util.List;
 @RequestMapping("/friends")
 public class FriendController {
 
+    static private MemberService memberService;
     static private FriendService friendService;
     static private AlarmService alarmService;
 
-    // 친구 추가하기
+    // 친구 요청하기
     @PostMapping
-    public ResponseEntity<Void> addFriend(@RequestParam("counterEmail") String email){
-
-        // token으로부터 memberId를 찾는 로직
-        int memberId;
-
-        int counterpart = friendService.insertFriend(memberId, email);
-        alarmService.requestFriendship(memberId, counterpart);
-
+    public ResponseEntity<Void> addFriend(FriendReqRequestDTO friendReqRequestDTO){
+        friendService.requestFriend(friendReqRequestDTO);
         return ResponseEntity.noContent().build();
     }
 
     // 친구 삭제하기
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> removeFriend(@RequestParam("id") String friendId){
-
-        // token으로부터 memberId를 찾는 로직
-        int memberId;
-
+    public ResponseEntity<Void> removeFriend(@RequestParam("id") int friendId){
         friendService.deleteFriend(friendId);
-
         return ResponseEntity.noContent().build();
     }
 
     // 친구 목록 불러오기
     @GetMapping
-    public ResponseEntity<List<Friend>> findFriends(){
-        //token으로부터 memberId를 찾는 로직
-        int memberId;
-
-        return ResponseEntity.ok(friendService.getFriends(memberId));
+    public ResponseEntity<List<FriendResponseDTO>> findFriends(){
+        return ResponseEntity.ok(friendService.getFriends());
     }
 
     // 친구 요청 목록 조회
     @GetMapping("/request")
-    public ResponseEntity<List<FriendRequest>> findRequestFriend(){
-        //token으로부터 memberId를 찾는 로직
-        int memberId;
-
-        return ResponseEntity.ok(friendService.getRequestFriend(memberId));
+    public ResponseEntity<List<FriendReqResponseDTO>> findRequestFriend(){
+        return ResponseEntity.ok(friendService.getRequestFriend());
     }
 
-    // 친구 수락하기
-    @PostMapping("/{id}/accept")
+    // 친구 요청 수락하기
+    @PostMapping("/accept/{id}")
     public ResponseEntity<Void> acceptFriendship(@PathVariable("id") int requestId){
-        // token 인증
-        if(true) {
-            friendService.insertFriendship(requestId);
-        }
-
+        friendService.insertFriendship(requestId);
         return ResponseEntity.noContent().build();
     }
 
-    // 친구 거절하기
-    @PostMapping("/{id}/reject")
+    // 친구 요청 거절하기
+    @PostMapping("/reject/{id}")
     public ResponseEntity<Void> rejectFriendship(@PathVariable("id") int requestId){
-        // token 인증
-        if(true){
-            friendService.deleteRequestFriend(requestId);
-        }
-
+        friendService.deleteRequestFriend(requestId);
         return ResponseEntity.noContent().build();
     }
-
-    // 친구 활동 피드
-
-    // 친구 일정 공유
 
 
 }
