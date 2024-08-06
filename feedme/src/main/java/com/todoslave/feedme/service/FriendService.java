5f5ec75979913bb91a92abf3a73c224ff5bd1740
김문희 <homoonshi@gmail.com>
2024-08-06 10:49:59 +0900

    Fix : friend 기능 수정

--- feedme/src/main/java/com/todoslave/feedme/service/FriendService.java
+++ feedme/src/main/java/com/todoslave/feedme/service/FriendService.java
@@ -1,23 +1,27 @@
 package com.todoslave.feedme.service;
 
+import com.todoslave.feedme.DTO.FriendReqRequestDTO;
+import com.todoslave.feedme.DTO.FriendReqResponseDTO;
+import com.todoslave.feedme.DTO.FriendResponseDTO;
 import com.todoslave.feedme.domain.entity.communication.Friend;
 import com.todoslave.feedme.domain.entity.communication.FriendRequest;
 
+import com.todoslave.feedme.domain.entity.membership.Member;
 import java.util.List;
 
 public interface FriendService {
 
     // 친구 추가
-    void requestFriend(int memberId, String email);
+    void requestFriend(FriendReqRequestDTO friendReqRequestDTO);
 
     // 친구 삭제
     void deleteFriend(int friendId);
 
     // 친구 목록 조회
-    List<Friend> getFriends(Integer memberId);
+    List<FriendResponseDTO> getFriends();
 
     // 친구 요청 목록 조회
-    List<FriendRequest> getRequestFriend(Integer memberId);
+    List<FriendReqResponseDTO> getRequestFriend();
 
     // 친구 수락
     void insertFriendship(int requestId);
--- feedme/src/main/java/com/todoslave/feedme/service/FriendServiceImpl.java
+++ feedme/src/main/java/com/todoslave/feedme/service/FriendServiceImpl.java
@@ -1,74 +1,124 @@
-//package com.todoslave.feedme.service;
-//
-//import com.todoslave.feedme.domain.entity.communication.Friend;
-//import com.todoslave.feedme.domain.entity.communication.FriendRequest;
-//import com.todoslave.feedme.domain.entity.membership.Member;
-//import com.todoslave.feedme.repository.FriendRepository;
-//import com.todoslave.feedme.repository.FriendRequestRepository;
-//import com.todoslave.feedme.repository.MemberRepository;
-//import jakarta.persistence.EntityNotFoundException;
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//
-//import java.util.List;
-//
-//@RequiredArgsConstructor
-//public class FriendServiceImpl implements FriendService{
-//
-//    MemberRepository memberRepository;
-//    FriendRepository friendRepository;
-//    FriendRequestRepository friendRequestRepository;
-//
-//    @Override
-//    public int requestFriend(int memberId, String email) {
-//
-//        int counterId;
-//
-//        FriendRequest friendRequest = new FriendRequest();
-//        friendRequest.setMember(memberId);
-//        friendRequest.setCounterpart_id(counterId);
-//
-//        friendRequestRepository.save(friendRequest);
-//
-//        return counterId;
-//    }
-//
-//    @Override
-//    @Transactional
-//    public void deleteFriend(int friendId) {
-//
-//        friendRequestRepository.deleteById(friendId);
-//
-//    }
-//
-//    @Override
-//    public List<Friend> getFriends(Integer memberId) {
-//        return friendRepository.findAllByMemberId(memberId);
-//    }
-//
-//    @Override
-//    public List<FriendRequest> getRequestFriend(Integer memberId) {
-//        return friendRequestRepository.findAllByMemberId(memberId);
-//    }
-//
-//    @Override
-//    public void insertFriendship(int id) {
-//
-//        FriendRequest friendRequest = friendRequestRepository.findById(id);
-//
-//        Friend friend = new Friend();
-//        Member member = friendRequest.getMember();
-//        friend.setMember(member);
-//        friend.setCounterpart_id(friendRequest.getCounterpart_id());
-//
-//        friendRepository.save(friend);
-//
-//    }
-//
-//    @Override
-//    public void deleteRequestFriend(int requestId) {
-//
-//        friendRequestRepository.deleteById(requestId);
-//
-//    }
-//}
+package com.todoslave.feedme.service;
+
+import com.todoslave.feedme.DTO.FriendReqRequestDTO;
+import com.todoslave.feedme.DTO.FriendReqResponseDTO;
+import com.todoslave.feedme.DTO.FriendResponseDTO;
+import com.todoslave.feedme.domain.entity.communication.Friend;
+import com.todoslave.feedme.domain.entity.communication.FriendRequest;
+import com.todoslave.feedme.domain.entity.membership.Member;
+import com.todoslave.feedme.login.util.SecurityUtil;
+import com.todoslave.feedme.repository.FriendRepository;
+import com.todoslave.feedme.repository.FriendRequestRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class FriendServiceImpl implements FriendService{
+
+    MemberService memberService;
+    FriendRepository friendRepository;
+    FriendRequestRepository friendRequestRepository;
+    MemberChatService memberChatService;
+
+    // 친구 요청
+    @Override
+    public void requestFriend(FriendReqRequestDTO friendReqRequestDTO) {
+
+        Member member = SecurityUtil.getCurrentMember();
+        Member counterpart = memberService.findByNickname(friendReqRequestDTO.getCounterpartNickname());
+
+        FriendRequest friendRequest = new FriendRequest();
+
+        friendRequest.setMember(member);
+        friendRequest.setCounterpartId(counterpart);
+
+        friendRequestRepository.save(friendRequest);
+
+    }
+
+    // 친구 삭제
+    @Override
+    @Transactional
+    public void deleteFriend(int friendId) {
+
+        friendRequestRepository.deleteById(friendId);
+
+    }
+
+    // 친구 목록 불러오기
+    @Override
+    public List<FriendResponseDTO> getFriends() {
+
+        int memberId = SecurityUtil.getCurrentUserId();
+
+        List<Friend> f = friendRepository.findAllByMemberId(memberId);
+        List<FriendResponseDTO> friends = new ArrayList<>();
+
+        for(Friend friend : f){
+
+            Member counterpart = friend.getCounterpart();
+            FriendResponseDTO friendResponseDTO = new FriendResponseDTO();
+            friendResponseDTO.setFriendId(friend.getId());
+            friendResponseDTO.setCounterpartNickname(counterpart.getNickname());
+            friends.add(friendResponseDTO);
+
+        }
+
+        return friends;
+    }
+
+    // 친구 요청 불러오기
+    @Override
+    public List<FriendReqResponseDTO> getRequestFriend() {
+
+        int memberId = SecurityUtil.getCurrentUserId();
+
+        List<FriendRequest> friendRequests = friendRequestRepository.findAllByMemberId(memberId);
+        List<FriendReqResponseDTO> friendReqResponseDTOList = new ArrayList<>();
+
+        for(FriendRequest friendRequest : friendRequests){
+
+            FriendReqResponseDTO friendReqResponseDTO = new FriendReqResponseDTO();
+            friendReqResponseDTO.setId(friendRequest.getId());
+            friendReqResponseDTO.setCounterpartNickname(friendRequest.getCounterpartId().getNickname());
+            friendReqResponseDTOList.add(friendReqResponseDTO);
+
+        }
+
+        return friendReqResponseDTOList;
+    }
+
+    // 친구 수락
+    @Override
+    public void insertFriendship(int requestId) {
+
+        FriendRequest friendRequest = friendRequestRepository.findById(requestId);
+
+        Friend friend = new Friend();
+        friend.setMember(SecurityUtil.getCurrentMember());
+        friend.setCounterpart(friendRequest.getCounterpartId());
+
+        friendRequestRepository.deleteById(requestId);
+
+        List<Integer> members = new ArrayList<>();
+        int memberId = SecurityUtil.getCurrentUserId();
+        members.add(memberId);
+        int counterpartId = friendRequest.getCounterpartId().getId();
+        members.add(counterpartId);
+
+        memberChatService.insertChatRoom(members);
+        friendRepository.save(friend);
+
+    }
+
+    // 친구 거절
+    @Override
+    public void deleteRequestFriend(int requestId) {
+
+        friendRequestRepository.deleteById(requestId);
+
+    }
+}
