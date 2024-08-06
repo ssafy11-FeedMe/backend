60636ff18a317741ebcd8d9fe7b1a0d158692851
김문희 <homoonshi@gmail.com>
2024-08-06 15:50:30 +0900

    Feat : Todo 일정 기능 추가

--- feedme/src/main/java/com/todoslave/feedme/DTO/TodoModifyRequestDTO.java
+++ feedme/src/main/java/com/todoslave/feedme/DTO/TodoModifyRequestDTO.java
@@ -3,9 +3,9 @@ package com.todoslave.feedme.DTO;
 import lombok.Getter;
 
 @Getter
-public class TodoCreateRequestDTO {
+public class TodoModifyRequestDTO {
 
+  private int id;
   private String content;
-  private int categoryId;
 
 }
