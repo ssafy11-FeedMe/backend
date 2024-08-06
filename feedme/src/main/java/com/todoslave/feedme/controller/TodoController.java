fa258ac912cd07f51e783f5335238ce4daed6701
김문희 <homoonshi@gmail.com>
2024-08-06 15:50:30 +0900

    Feat : Todo 일정 기능 추가

--- feedme/src/main/java/com/todoslave/feedme/controller/TodoController.java
+++ feedme/src/main/java/com/todoslave/feedme/controller/TodoController.java
@@ -1,9 +1,13 @@
 package com.todoslave.feedme.controller;
 
-import com.todoslave.feedme.domain.entity.task.Todo;
+import com.todoslave.feedme.DTO.TodoCalendarResponseDTO;
+import com.todoslave.feedme.DTO.TodoCreateRequestDTO;
+import com.todoslave.feedme.DTO.TodoDailyRequestDTO;
+import com.todoslave.feedme.DTO.TodoMainResponseDTO;
+import com.todoslave.feedme.DTO.TodoModifyRequestDTO;
+import com.todoslave.feedme.DTO.TodoRequestDTO;
+import com.todoslave.feedme.DTO.TodoResponseDTO;
 import com.todoslave.feedme.service.TodoService;
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,33 +27,61 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
 
   @Autowired
-  private final TodoService todoservice;
+  private final TodoService todoService;
 
-  @GetMapping("/daily")
-  public ResponseEntity<List<Todo>> findTodoDaily(@RequestBody Todo todo){
-    Date timestamp = Date(todo.getCreatedAt().get());
-    return ResponseEntity.ok(todoservice.getTodoDaily(todo.getMember().getId(), createdAt));
+  // 할일 목록에서 일정(일) 불러오기
+  @GetMapping("/todolist/daily")
+  public ResponseEntity<List<TodoResponseDTO>> findDailyTodoList(@RequestBody
+                      TodoDailyRequestDTO todoDailyRequestDTO){
+
+    return ResponseEntity.ok(todoService.getTodoListDaily(todoDailyRequestDTO));
   }
 
+  // 메인 달력에서 일정 불러오기
+  @GetMapping("/calendar/daily")
+  public ResponseEntity<List<TodoResponseDTO>> findCalendarTodoList(@RequestBody TodoRequestDTO todoRequestDTO){
+
+    return ResponseEntity.ok(todoService.getTodoCalendarDaily(todoRequestDTO));
+  }
+
+  // 메인 화면에서 당일 안한 일정들 불러오기
+  @GetMapping("/main/daily")
+  public ResponseEntity<List<TodoMainResponseDTO>> findMainInCompleted(){
+
+    return ResponseEntity.ok(todoService.getTodoMainDaily());
+  }
+
+  // 월별 일정 완/미완 불러오기
+  @GetMapping("/calendar")
+  public ResponseEntity<List<TodoCalendarResponseDTO>> findCalendarIsCompleted(@RequestBody TodoRequestDTO todoRequestDTO){
+
+    return ResponseEntity.ok(todoService.getTodoCalendarCompleted(todoRequestDTO));
+  }
+
+
+  // 투두 생성
   @PostMapping
-  public ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
-    return ResponseEntity.ok(todoservice.insertTodo(todo));
+  public ResponseEntity<TodoResponseDTO> createTodo(@RequestBody TodoCreateRequestDTO todo){
+    return ResponseEntity.ok(todoService.insertTodo(todo));
   }
 
+  // 투두 삭제
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> removeTodo(@PathVariable int id){
-    todoservice.deleteTodo(id);
+    todoService.deleteTodo(id);
     return ResponseEntity.noContent().build();
   }
 
+  // 투두 수정
   @PatchMapping()
-  public ResponseEntity<Todo> modifyTodo(@RequestBody Todo todo){
-    return ResponseEntity.ok(todoservice.updateTodoContent(todo.getId(), todo.getContent()));
+  public ResponseEntity<TodoResponseDTO> modifyTodo(@RequestBody TodoModifyRequestDTO todoModifyRequestDTO){
+    return ResponseEntity.ok(todoService.updateTodo(todoModifyRequestDTO));
   }
 
-  @PostMapping("/{id}/complete")
-  public ResponseEntity<Todo> completeTodo(@PathVariable int id){
-    return ResponseEntity.ok(todoservice.completeTodo(id));
+  // 투두 완료
+  @PostMapping("/complete/{id}")
+  public ResponseEntity<TodoResponseDTO> completeTodo(@PathVariable int id){
+    return ResponseEntity.ok(todoService.completeTodo(id));
   }
 
 }
--- feedme/src/main/java/com/todoslave/feedme/repository/TodoRepository.java
+++ feedme/src/main/java/com/todoslave/feedme/repository/TodoRepository.java
@@ -14,9 +14,16 @@ import org.springframework.stereotype.Repository;
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
 
   @Query("SELECT t FROM Todo t WHERE t.member.id = :memberId AND FUNCTION('DATE', t.createdAt) = :createdAt")
-  List<Todo> findAllByMemberIdAndCreatedAt(@Param("memberId") int memberId, @Param("createdAt") Date createdAt);
+  List<Todo> findAllByMemberIdAndCreatedAt(@Param("memberId") int memberId, @Param("createdAt") LocalDate createdAt);
+
+  @Query("SELECT t FROM Todo t WHERE t.member.id = :memberId AND FUNCTION('DATE', t.createdAt) = :createdAt AND t.isCompleted = :isCompleted")
+  List<Todo> findAllByMemberIdAndCreatedAtIsCompleted(@Param("memberId") int memberID, @Param("createdAt") LocalDate createdAt,@Param("isCompleted") int isCompleted);
 
   @Query("SELECT t.member.id FROM Todo t WHERE t.createdAt = :createdAt AND t.isCompleted = 0")
   List<Integer> findMemberIdAllByCreatedAtAndIsCompleted(LocalDate createdAt);
+
+  @Query("SELECT COUNT(t) FROM Todo t WHERE t.createdAt = :date AND t.isCompleted = :isCompleted")
+  long countTodoByDateAndIsCompleted(@Param("date") LocalDate date, @Param("isCompleted") int isCompleted);
+
 
 }
