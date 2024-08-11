c4b968a9e61cb723afefb6918ee1638ce4d3604b
yebon <goodyebon1@gmail.com>
2024-08-11 23:24:22 +0900

    feat : 기능

--- feedme/src/main/java/com/todoslave/feedme/DTO/FeedDTO.java
+++ feedme/src/main/java/com/todoslave/feedme/DTO/FeedDTO.java
@@ -8,11 +8,14 @@ import java.util.List;
 @Data
 public class FeedDTO {
         private int feedId;
+        private String email;
         private String nickname;
         private String img;
         private String caption;
         private LocalDateTime lastCreateTime;
         private int likes;
+
+
         private List<FeedCommentDTO> comments;
     }
 
